e101dbf259c32193e23d38b7cd213150d95f4409
matvei_ilin_1 <ilinms.3007@mail.ru>
2025-12-28 19:40:37 +0300

    Add first page object and create issue test

--- pages/issues_page.py
+++ pages/issues_page.py
@@ -12,7 +12,7 @@ class IssuesPage:
         self.page.wait_for_load_state("networkidle")
 
     def create_issue(self, title: str, description: str):
-        self.page.get_by_role("button", name="Create issue").click()
+        self.page.get_by_role("button", name="Создать задачу").click()
         self.page.get_by_label("Title").fill(title)
         self.page.get_by_label("Description").fill(description)
         self.page.get_by_role("button", name="Create").click()
